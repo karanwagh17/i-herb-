a26b1e1c6653a14d362826d67f2c58caf024da41
karan wagh <167848916+karanwagh17@users.noreply.github.com>
2025-06-18 21:33:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors({
-  origin: ["https://i-herb-ten.vercel.app","http://localhost:5173"]
+  // origin: ["https://i-herb-ten.vercel.app","http://localhost:5173"]
+  origin : "http://localhost:5173"
   credentials: true,
 }));
 
